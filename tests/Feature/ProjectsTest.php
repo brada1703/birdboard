e0b2de9526d1f9a57003c653cdf17f5d8f6e717e
Brad <bradley.ahrens@kwanko.com>
2020-02-15 20:53:47 +0000

    chore: finish project creation in controller

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -41,4 +41,16 @@ class ProjectsTest extends TestCase
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
+    /** @test */
+    public function a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+
+        $this->get('/projects/'.$project->id)
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
 }
